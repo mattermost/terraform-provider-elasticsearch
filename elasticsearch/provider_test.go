7eebd7e27b14a1fe15631ab1d46335b854ea4d90
Spiros Economakis <spiros.oikonomakis@gmail.com>
2021-04-30 12:48:41 +0300

    Add the new terraform elasticsearch provider
    This is the provider we use to run operations with terraform for our ES instances.
    One of the current use cases is create templates for the index patterns we want
    to have. This till now was happening manually with simple bash scripts or curl
    commands.
    
    Now it will be part of our terraform recipes.
    
    Ticket: https://mattermost.atlassian.net/browse/MM-35109

---
+++ elasticsearch/provider_test.go
@@ -0,0 +1,34 @@
+package elasticsearch
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+var testAccProviders map[string]*schema.Provider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider()
+	testAccProviders = map[string]*schema.Provider{
+		"elasticsearch": testAccProvider,
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvider_impl(t *testing.T) {
+	var _ *schema.Provider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	if err := os.Getenv("ES_URL"); err == "" {
+		t.Fatal("ES_URL must be set for acceptance tests")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/mattermost/terraform-provider-elasticsearch
+
+go 1.16
+
+require (
+	github.com/elastic/go-elasticsearch/v7 v7.12.0
+	github.com/hashicorp/hcl/v2 v2.8.2 // indirect
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.6.1
+	github.com/pkg/errors v0.9.1
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/genproto v0.0.0-20200904004341-0bd0a958aa1d // indirect
+)
